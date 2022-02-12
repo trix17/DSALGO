e4bbed8c3cea89a82d3365b29a6aaf7831cdca16
trix17 <laurentjean90@yahoo.fr>
2022-02-12 18:36:06 -0500

    add comments to insertion sort algo

--- sorting/bubblesort.py
+++ sorting/bubblesort.py
@@ -19,4 +19,18 @@ for i in range(len(n)):
     print(n[i])
 
 
+"""
+I have alway had problem with nested loop 
+basic logic so I can remmeber, next time I revisit Bubblesort
 
+the outter loop keeps track of how many time we will run this program , 
+the inner loop is all the numbers part of the array that will be printed
+n amount of times  (outter loop ). 
+before executing the algorithm 
+if we just print n[j] of the outter loop 
+the result will be [-2,45,0,11,-9], [-2,45,0,11,-9], [-2,45,0,11,-9], [-2,45,0,11,-9], [-2,45,0,11,-9]
+until we start moving numbers in ascending order. 
+
+
+
+"""
